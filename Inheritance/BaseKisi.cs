5c6a6bd63e4a49b64e363bf4da60efbe0e536208
Ceydaay <cydylmz.99@gmail.com>
2024-08-13 22:53:34 +0300

    Add project files.

---
+++ Inheritance/BaseKisi.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+    public class BaseKisi
+    {
+        public string ad {  get; set; }
+        public string soyad { get; set; }
+
+        public void Yazdir ()
+        {
+            Console.WriteLine($"Ad:{ad} Soyad:{soyad}");
+        }
+
+        public class Ogrenci: BaseKisi
+        {
+            public string OgrenciNumarasi { get; set; }
+
+            public void OgrenciYazdir()
+            {
+                Console.WriteLine($"Öğrenci Numarası: {OgrenciNumarasi}");
+                Yazdir();
+            }
+
+        }
+
+        public class Ogretmen : BaseKisi
+        {
+            public string MaasBilgisi { get; set; }
+
+            public void OgretmenYazdir()
+            {
+                Console.WriteLine($"Öğrenci Numarası: {MaasBilgisi}");
+                Yazdir();
+
+            }
+        }
+
+    }
+}
---
+++ Inheritance/Program.cs
@@ -0,0 +1,30 @@
+﻿using static Inheritance.BaseKisi;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        // Ogrenci nesnesi oluşturma ve değer atama
+        Ogrenci ogrenci = new Ogrenci
+        {
+            ad = "Ahmet",
+            soyad = "Yılmaz",
+            OgrenciNumarasi = "123456"
+        };
+
+        // Ogretmen nesnesi oluşturma ve değer atama
+        Ogretmen ogretmen = new Ogretmen
+        {
+            ad = "Ayşe",
+            soyad = "Kara",
+            MaasBilgisi = "7500.50m"
+        };
+
+        // Ogrenci ve Ogretmen nesnelerini yazdırma
+        Console.WriteLine("Öğrenci Bilgileri:");
+        ogrenci.OgrenciYazdir();
+
+        Console.WriteLine("\nÖğretmen Bilgileri:");
+        ogretmen.OgretmenYazdir();
+    }
+}
